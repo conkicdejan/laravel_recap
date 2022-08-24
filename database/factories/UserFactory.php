0534e7fd35c03e53523da21d2e5c8843254c7619
Dejan Conkic <conkicdejan@gmail.com>
2022-08-24 17:12:47 +0200

    Add select country to register form

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,6 +18,7 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
+            'name' => fake()->name(),
             'first_name' => fake()->name(),
             'last_name' => fake()->name(),
             'email' => fake()->safeEmail(),
