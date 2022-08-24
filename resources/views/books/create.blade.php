b58b610841b05d341c76b78a7ef892dfea78d197
Dejan Conkic <conkicdejan@gmail.com>
2022-08-24 13:29:29 +0200

    Fix - merge create/edit form

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -4,19 +4,26 @@
 
 @section('content')
 
-<form action="/books" method="post">
+
+@if (isset($book))
+    <form action="{{ '/books/'. $book->id }}" method="post">
+    @method("PUT")
+@else
+    <form action="/books" method="post">
+@endif
+
     @csrf
 
     <label for="title">Title</label>
-    <input type="text" name='title' id='title'>
+    <input type="text" name='title' id='title' value="{{ isset($book) ? $book->title : ''}}">
 
     <label for="content">Content</label>
-    <input type="text" name='content' id='content'>
+    <input type="text" name='content' id='content' value="{{ isset($book) ? $book->content : ''}}">
 
     <label for="user_id">User Id</label>
-    <input type="number" name='user_id' id='user_id'>
+    <input type="number" name='user_id' id='user_id' value="{{ isset($book) ? $book->user_id : ''}}">
 
-    <button type='submit'>Create</button>
+    <button type='submit'>{{ isset($book) ? "Edit" : "Create"}}</button>
 
 </form>
  @endsection
