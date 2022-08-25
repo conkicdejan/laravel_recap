c79293218cdf3036cf39e2ad1c9a6d73046298db
Dejan Conkic <conkicdejan@gmail.com>
2022-08-25 13:31:20 +0200

    Add - Post CRUD & Policy

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,11 @@ Route::get('/', function () {
 Route::resource('cars', CarController::class);
 
 Route::controller(PostController::class)->group(function () {
-    Route::get('/posts', 'index')->name('posts.index');
-    Route::post('/posts', 'store')->name('posts.store');
-    Route::get('/posts/{post}', 'show')->name('posts.show');
-    Route::put('/posts/{post}', 'update')->name('posts.update');
-    Route::delete('/posts/{post}', 'destroy')->name('posts.destroy');
+    Route::get('posts', 'index')->name('posts.index');
+    Route::get('posts/create', 'create')->name('posts.create');
+    Route::post('posts', 'store')->name('posts.store');
+    Route::get('posts/{post}', 'show')->name('posts.show');
+    Route::get('posts/{post}/edit', 'edit')->name('posts.edit');
+    Route::put('posts/{post}', 'update')->name('posts.update');
+    Route::delete('posts/{post}', 'destroy')->name('posts.destroy');
 });
